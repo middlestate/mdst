238431cd25fcb5502b2e1cd9b0d592fcf92cbb2d
datahavest <ryan@dataharvest.co>
2018-12-20 15:06:31 -0800

    pushing template

---
+++ js/kana.js
@@ -0,0 +1,226 @@
+jQuery( document ).ready(function( $ ) {
+"use strict"
+/*-----------------------------------------------------------------------------------*/
+/* 	LOADER
+/*-----------------------------------------------------------------------------------*/
+$("#loader").delay(500).fadeOut("slow");
+/*-----------------------------------------------------------------------------------*/
+/*		STICKY NAVIGATION
+/*-----------------------------------------------------------------------------------*/
+$(".sticky").sticky({topSpacing:0});
+/*-----------------------------------------------------------------------------------*/
+/*  FULL SCREEN
+/*-----------------------------------------------------------------------------------*/
+$('.full-screen').superslides({});
+/*-----------------------------------------------------------------------------------
+    Animated progress bars
+/*-----------------------------------------------------------------------------------*/
+$('.progress-bars').waypoint(function() {
+  $('.progress').each(function(){
+    $(this).find('.progress-bar').animate({
+      width:$(this).attr('data-percent')
+     },200);
+});},
+	{ 
+	offset: '100%',
+    triggerOnce: true 
+});
+/*-----------------------------------------------------------------------------------*/
+/*    Parallax
+/*-----------------------------------------------------------------------------------*/
+jQuery.stellar({
+   horizontalScrolling: false,
+   scrollProperty: 'scroll',
+   positionProperty: 'position',
+});
+/*-----------------------------------------------------------------------------------*/
+/* 	SLIDER REVOLUTION
+/*-----------------------------------------------------------------------------------*/
+jQuery('.tp-banner').show().revolution({
+	dottedOverlay:"none",
+	delay:10000,
+	startwidth:1170,
+	startheight:700,
+	navigationType:"bullet",
+	navigationArrows:"solo",
+	navigationStyle:"preview4",
+	parallax:"mouse",
+	parallaxBgFreeze:"on",
+	parallaxLevels:[7,4,3,2,5,4,3,2,1,0],												
+	keyboardNavigation:"on",						
+	shadow:0,
+	fullWidth:"on",
+	fullScreen:"on",
+	shuffle:"off",						
+	autoHeight:"off",						
+	forceFullWidth:"off",	
+	fullScreenOffsetContainer:""	
+});
+/*-----------------------------------------------------------------------------------*/
+/* 	TESTIMONIAL SLIDER
+/*-----------------------------------------------------------------------------------*/
+$("#testi-slide").owlCarousel({ 
+    items : 1,
+	autoplay:true,
+	loop:true,
+	autoplayTimeout:5000,
+	autoplayHoverPause:true,
+	singleItem	: true,
+    navigation : false,
+	navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+	pagination : true,
+	animateOut: 'fadeOut'	
+});
+/*-----------------------------------------------------------------------------------
+    TESTNMONIALS STYLE 1
+/*-----------------------------------------------------------------------------------*/
+$('.home-slide').flexslider({
+	mode: 'fade',
+	auto: true
+});
+/*-----------------------------------------------------------------------------------*/
+/* 	ANIMATION
+/*-----------------------------------------------------------------------------------*/
+var wow = new WOW({
+    boxClass:     'animat',      // animated element css class (default is wow)
+    animateClass: 'animat', // animation css class (default is animated)
+    offset:       100,          // distance to the element when triggering the animation (default is 0)
+    mobile:       false        // trigger animations on mobile devices (true is default)
+});
+wow.init();
+/*-----------------------------------------------------------------------------------*/
+/*	CUBE PORTFOLIO
+/*-----------------------------------------------------------------------------------*/
+$('#js-grid-awesome-work').cubeportfolio({
+    filters: '#js-filters-awesome-work',
+    loadMore: '#js-loadMore-awesome-work',
+    loadMoreAction: 'click',
+    layoutMode: 'grid',
+    defaultFilter: '*',
+    animationType: 'quicksand',
+    gapHorizontal: 0,
+    gapVertical: 0,
+    gridAdjustment: '',
+    caption: 'zoom',
+    displayType: 'lazyLoading',
+    displayTypeSpeed: 400,
+// singlePage popup
+    singlePageDelegate: '.cbp-singlePage',
+    singlePageDeeplinking: true,
+    singlePageStickyNavigation: true,
+    singlePageCounter: '<div class="cbp-popup-singlePage-counter">{{current}} of {{total}}</div>',
+    singlePageCallback: function(url, element) {
+// to update singlePage content use the following method: this.updateSinglePage(yourContent)
+    var t = this;
+       $.ajax({
+         url: url,
+         type: 'GET',
+         dataType: 'html',
+         timeout: 10000
+    })
+.done(function(result) {
+         t.updateSinglePage(result);
+         })
+.fail(function() {
+          t.updateSinglePage('AJAX Error! Please refresh the page!');
+     });
+    },
+});
+});
+/*-----------------------------------------------------------------------------------*/
+/*    CONTACT FORM
+/*-----------------------------------------------------------------------------------*/
+function checkmail(input){
+  var pattern1=/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+  	if(pattern1.test(input)){ return true; }else{ return false; }}     
+    function proceed(){
+    	var name = document.getElementById("name");
+		var email = document.getElementById("email");
+		var company = document.getElementById("company");
+		var msg = document.getElementById("message");
+		var errors = "";
+		if(name.value == ""){ 
+		name.className = 'error';
+	  	  return false;}    
+		  else if(email.value == ""){
+		  email.className = 'error';
+		  return false;}
+		    else if(checkmail(email.value)==false){
+		        alert('Please provide a valid email address.');
+		        return false;}
+		    else if(company.value == ""){
+		        company.className = 'error';
+		        return false;}
+		   else if(msg.value == ""){
+		        msg.className = 'error';
+		        return false;}
+		   else 
+		  {
+	$.ajax({
+		type: "POST",
+		url: "php/submit.php",
+		data: $("#contact_form").serialize(),
+		success: function(msg){
+		//alert(msg);
+		if(msg){
+			$('#contact_form').fadeOut(1000);
+			$('#contact_message').fadeIn(1000);
+				document.getElementById("contact_message");
+			 return true;
+		}}
+	});
+}};
+/*-----------------------------------------------------------------------------------*/
+/*	Go TO TOP
+/*-----------------------------------------------------------------------------------*/
+var offset = 1300,
+	//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+	offset_opacity = 1500,
+	//duration of the top scrolling animation (in ms)
+	scroll_top_duration = 1000,
+	//grab the "back to top" link
+	$back_to_top = $('.cd-top');
+
+//hide or show the "back to top" link
+$(window).scroll(function(){
+	( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+	if( $(this).scrollTop() > offset_opacity ) { 
+		$back_to_top.addClass('cd-fade-out');
+	}
+});
+//smooth scroll to top
+$back_to_top.on('click', function(event){
+	event.preventDefault();
+	$('body,html').animate({
+		scrollTop: 0 ,
+	 	}, scroll_top_duration
+);
+});
+
+/*-----------------------------------------------------------------------------------*/
+/* 		Active Menu Item on Page Scroll
+/*-----------------------------------------------------------------------------------*/
+$(window).scroll(function(event) {
+		Scroll();
+});	
+$('.scroll a').click(function() {  
+	$('html, body').animate({scrollTop: $(this.hash).offset().top -0}, 1000);
+		return false;
+});
+// User define function
+function Scroll() {
+var contentTop      =   [];
+var contentBottom   =   [];
+var winTop      =   $(window).scrollTop();
+var rangeTop    =   5;
+var rangeBottom =   1000;
+$('nav').find('.scroll a').each(function(){
+	contentTop.push( $( $(this).attr('href') ).offset().top);
+		contentBottom.push( $( $(this).attr('href') ).offset().top + $( $(this).attr('href') ).height() );
+})
+$.each( contentTop, function(i){
+if ( winTop > contentTop[i] - rangeTop ){
+	$('nav li.scroll')
+	  .removeClass('active')
+		.eq(i).addClass('active');			
+}}  )};
